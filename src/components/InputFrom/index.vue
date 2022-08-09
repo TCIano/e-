26f942ae54d34960ff855ee4b6248a0d9170bdb7
Mynmhui <286734637@qq.com>
2022-08-09 14:53:02 +0800

    1

--- src/components/InputFrom/index.vue
+++ src/components/InputFrom/index.vue
@@ -2,19 +2,12 @@
   <div class="search">
     <el-form :inline="true" class="demo-form-inline">
       <!-- <slot></slot> -->
-
-
-      <el-form-item label="工单编号">
-
       <el-form-item :label="inputInfo.one" v-if="inputInfo">
         <el-input
           placeholder="请输入"
           v-model="searchContion.taskCode"
         ></el-input>
       </el-form-item>
-
-      <el-form-item label="工单状态">
-
       <el-form-item :label="inputInfo.two" v-if="!isSingle">
         <el-select placeholder="请选择" v-model="searchContion.status">
           <el-option
@@ -76,3 +69,4 @@ export default {
   margin-bottom: 30px;
 }
 </style>
+
