8689a839c53f692f70fa496f904f2e88bfbdb21b
TCIano <740409310@qq.com>
2022-08-10 20:35:38 +0800

    首页

--- src/api/humanTask.js
+++ src/api/humanTask.js
@@ -11,3 +11,41 @@ export const TaskInfoApi = (start, end) => {
     url: "/task-service/task/taskReportInfo/" + start + "/" + end,
   });
 };
+
+/**
+ *获取前几的商品
+ * @param {String} start
+ * @param {String} end
+ * @returns
+ */
+export const getTopSale = (start, end) => {
+  return request({
+    url: "/order-service/report/skuTop/" + 10 + "/" + start + "/" + end,
+  });
+};
+
+/**
+ *获取一定时间的订单数
+ * @param {Object} params
+ * start	是	2020-01-01 00:00:00	开始日期时间
+ *end	是	2020-01-01 00:00:00	结束日期时间
+ * @returns
+ */
+export const getOrderCountApi = (params) => {
+  return request({
+    url: "/order-service/report/orderCount",
+    params,
+  });
+};
+
+/**
+ * 获取销售总额
+ * @param {Object} params
+ * @returns
+ */
+export const getSaleCountApi = (params) => {
+  return request({
+    url: "/order-service/report/orderAmount",
+    params,
+  });
+};
